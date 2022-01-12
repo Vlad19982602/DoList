e9671c7d65d6a976fac4c198d62ab7015c29f846
Vlad19982602 <79852880+Vlad19982602@users.noreply.github.com>
2022-01-12 22:48:38 +0500

    Add project files.

---
+++ DoList/MainWindow.xaml.cs
@@ -0,0 +1,100 @@
+﻿using DoList.Models;
+using DoList.Services;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DoList
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private readonly string PATH = $"{Environment.CurrentDirectory}\\todoData.json";
+        private BindingList<ToDoModel> _todoData;
+        private FileIOService _fileIOService;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            _fileIOService = new FileIOService(PATH);
+
+            try
+            {
+                _todoData = _fileIOService.LoadData();
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.Message);
+                Close();
+                    
+            }
+            
+
+
+            dgDoList.ItemsSource = _todoData;
+            _todoData.ListChanged += _todoData_ListChanged;
+        }
+
+        private void _todoData_ListChanged(object sender, ListChangedEventArgs e)
+        {
+
+            if(e.ListChangedType == ListChangedType.ItemAdded || e.ListChangedType == ListChangedType.ItemDeleted || e.ListChangedType == ListChangedType.ItemMoved)
+            {
+                try
+                {
+                    _fileIOService.SaveData(sender);
+                }
+                catch (Exception ex)
+                {
+
+                    MessageBox.Show(ex.Message);
+                    Close();
+
+                }
+            }
+
+
+
+
+           // switch (e.ListChangedType)
+           // {
+           //     case ListChangedType.Reset:
+           //         break;
+           //     case ListChangedType.ItemAdded:
+           //         break;
+           //     case ListChangedType.ItemDeleted:
+           //         break;
+           //     case ListChangedType.ItemMoved:
+           //         break;
+           //     case ListChangedType.PropertyDescriptorAdded:
+           //         break;
+           //     case ListChangedType.PropertyDescriptorChanged:
+           //         break;
+           //     case ListChangedType.PropertyDescriptorDeleted:
+           //         break;
+           //     default:
+           //         break;
+           // }
+            
+        }
+    }
+}
---
+++ DoList/Models/ToDoModel.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+
+namespace DoList.Models
+{
+    class ToDoModel : INotifyPropertyChanged
+    {
+        public DateTime CreationDate { get; set; } = DateTime.Now;
+
+        private bool _isDone;
+
+        public bool IsDone
+        {
+            get { return _isDone; }
+            set
+            {
+                if(_isDone == value)
+                {
+                    return;
+                }
+                _isDone = value;
+                OnPropertyChanged("IsDone");
+            }
+        }
+
+        private string _text;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public string Text
+        {
+            get { return _text; }
+            set 
+            {
+                if(_text == value)
+                {
+                    return;
+                }
+                _text = value;
+                OnPropertyChanged("Text");
+            }
+        }
+
+        protected virtual void OnPropertyChanged(string propertyName = "")
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+            
+        }
+
+    }
+}
